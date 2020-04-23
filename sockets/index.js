f42e5d48939a7bb0f76159e4b8d683d6a97bc7d3
Necmettin <necmettinyanik@outlook.com>
2020-04-23 22:45:35 +0300

    socket configuration fixed

--- sockets/index.js
+++ sockets/index.js
@@ -1,22 +1,21 @@
 module.exports = (io) => {
   io.on('connection', async (socket) => {
-
     socket.broadcast.on('addNewBrand', (isSuccess) => {
-      if(isSuccess){
-          socket.broadcast.emit('getBrand', true);
-      }      
+      if (isSuccess) {
+        io.emit('getBrand', true);
+      }
     });
 
-     socket.broadcast.on('updateBrand', (isSuccess) => {
-      if(isSuccess){
-          socket.broadcast.emit('getBrand', true);
-      }      
+    socket.broadcast.on('updateBrand', (isSuccess) => {
+      if (isSuccess) {
+        io.emit('getBrand', true);
+      }
     });
 
-     socket.broadcast.on('deleteBrand', (isSuccess) => {
-      if(isSuccess){
-          socket.broadcast.emit('getBrand', true);
-      }      
+    socket.broadcast.on('deleteBrand', (isSuccess) => {
+      if (isSuccess) {
+        io.emit('getBrand', true);
+      }
     });
   });
 };
